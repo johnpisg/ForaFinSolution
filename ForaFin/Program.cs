adcf74b66a640fd57d0e51de802306fd77231e53
johnpisg <johnpisg@gmail.com>
2026-02-10 08:03:48 -0600

    Jhon: program class

--- ForaFin/Program.cs
+++ ForaFin/Program.cs
@@ -14,55 +14,62 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
-var builder = FunctionsApplication.CreateBuilder(args);
-
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Information()
-    .Enrich.FromLogContext()
-    .WriteTo.Console()
-    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day) // Log local en archivos
-    .CreateLogger();
-
-builder.Services.AddLogging(loggingBuilder =>
+public class Program
 {
-    loggingBuilder.AddSerilog(dispose: true);
-});
-
-builder.ConfigureFunctionsWebApplication();
-
-builder.Services
-    .AddApplicationInsightsTelemetryWorkerService()
-    .ConfigureFunctionsApplicationInsights();
-
-builder.Services.AddDbContext<ForaFinDb>(opt =>
-    opt
-    // .UseSqlServer("connString", sqlOptions => sqlOptions.EnableRetryOnFailure())
-    .UseInMemoryDatabase("ForaFinDb")
-    .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning))
-);
-builder.Services.AddScoped<ITokenValidator, TokenValidator>();
-builder.Services.AddScoped<IForaFinRepository, ForaFinRepository>();
-builder.Services.AddScoped<ISecEdgarService, SecEdgarService>();
-builder.Services.AddScoped<ICompanyService, CompanyService>();
-builder.Services.AddSingleton(new JsonSerializerOptions
-{
-    PropertyNameCaseInsensitive = true,
-    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-    Converters =
+    public static void Main(string[] args)
     {
-        new JsonStringEnumConverter(JsonNamingPolicy.CamelCase),
-        new PaddedIntConverter()
-    }
-});
-builder.Services.AddHttpClient("ExternalApi", client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("SecEdgarApiBaseUrl") ?? "");
-    client.Timeout = TimeSpan.FromSeconds(30);
-    client.DefaultRequestHeaders.Add("User-Agent", "PostmanRuntime/7.34.0");
-    client.DefaultRequestHeaders.Add("Accept", "*/*");
-});
-builder.Services.AddProblemDetails();
+        var builder = FunctionsApplication.CreateBuilder(args);
 
-var app = builder.Build();
-app.Run();
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day) // Log local en archivos
+            .CreateLogger();
+
+        builder.Services.AddLogging(loggingBuilder =>
+        {
+            loggingBuilder.AddSerilog(dispose: true);
+        });
+
+        builder.ConfigureFunctionsWebApplication();
+
+        builder.Services
+            .AddApplicationInsightsTelemetryWorkerService()
+            .ConfigureFunctionsApplicationInsights();
+
+        builder.Services.AddDbContext<ForaFinDb>(opt =>
+            opt
+            // .UseSqlServer("connString", sqlOptions => sqlOptions.EnableRetryOnFailure())
+            .UseInMemoryDatabase("ForaFinDb")
+            .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+        );
+        builder.Services.AddScoped<ITokenValidator, TokenValidator>();
+        builder.Services.AddScoped<IForaFinRepository, ForaFinRepository>();
+        builder.Services.AddScoped<ISecEdgarService, SecEdgarService>();
+        builder.Services.AddScoped<ICompanyService, CompanyService>();
+        builder.Services.AddSingleton(new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            Converters =
+            {
+                new JsonStringEnumConverter(JsonNamingPolicy.CamelCase),
+                new PaddedIntConverter()
+            }
+        });
+        builder.Services.AddHttpClient("ExternalApi", client =>
+        {
+            client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("SecEdgarApiBaseUrl") ?? "");
+            client.Timeout = TimeSpan.FromSeconds(30);
+            client.DefaultRequestHeaders.Add("User-Agent", "PostmanRuntime/7.34.0");
+            client.DefaultRequestHeaders.Add("Accept", "*/*");
+        });
+        builder.Services.AddProblemDetails();
+
+        var app = builder.Build();
+        app.Run();
+
+    }
+}
